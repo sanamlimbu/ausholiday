9c88d999af19f8d3fe67a10d2345e79d86ccb409
Sanam Limbu <sudosanam@gmail.com>
2023-10-04 15:34:50 +0800

    project init

---
+++ index.test.js
@@ -0,0 +1,64 @@
+const { isPublicHoliday, isWeekend, dateAfterWorkingDays } = require(".");
+
+describe("test public holiday", () => {
+  test("2023/09/25 is public holiday", () => {
+    expect(isPublicHoliday(new Date("2023/09/25"))).toBe(true);
+  });
+  test("2025/12/26 is public holiday", () => {
+    expect(isPublicHoliday(new Date("2025/12/26"))).toBe(true);
+  });
+  test("2024/03/31 is public holiday", () => {
+    expect(isPublicHoliday(new Date("2024/03/31"))).toBe(true);
+  });
+
+  test("2024/03/23 is not public holiday", () => {
+    expect(isPublicHoliday(new Date("2024/03/23"))).toBe(false);
+  });
+  test("2025/03/7 is not public holiday", () => {
+    expect(isPublicHoliday(new Date("2025/03/07"))).toBe(false);
+  });
+});
+
+describe("test weekends", () => {
+  test("2023/10/07 is weekend", () => {
+    expect(isWeekend(new Date("2023/10/07"))).toBe(true);
+  });
+
+  test("2023/10/22 is weekend", () => {
+    expect(isWeekend(new Date("2023/10/22"))).toBe(true);
+  });
+
+  test("2023/10/11 is not weekend", () => {
+    expect(isWeekend(new Date("2023/10/11"))).toBe(false);
+  });
+
+  test("2023/10/04 is not weekend", () => {
+    expect(isWeekend(new Date("2023/10/04"))).toBe(false);
+  });
+});
+
+describe("test dates after no of working days", () => {
+  test("10 workings after 2023/09/18", () => {
+    expect(
+      dateAfterWorkingDays(new Date("2023/09/18"), 10).toDateString()
+    ).toBe(new Date("2023/10/03").toDateString());
+  });
+
+  test("5 workings after 2023/09/18", () => {
+    expect(dateAfterWorkingDays(new Date("2023/09/18"), 5).toDateString()).toBe(
+      new Date("2023/09/26").toDateString()
+    );
+  });
+
+  test("20 working days after 2023/12/20", () => {
+    expect(
+      dateAfterWorkingDays(new Date("2023/12/20"), 20).toDateString()
+    ).toBe(new Date("2024/01/22").toDateString());
+  });
+
+  test("20 working days after 2023/12/15", () => {
+    expect(
+      dateAfterWorkingDays(new Date("2023/12/15"), 20).toDateString()
+    ).toBe(new Date("2024/01/17").toDateString());
+  });
+});
