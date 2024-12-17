3a3d566092d0c4f6d635f499e899fcc00115439b
Sanam Limbu <sudosanam@gmail.com>
2024-12-17 15:18:44 +0800

    added jurisdictions and common public holidays

---
+++ src/types.ts
@@ -0,0 +1,24 @@
+// All states and territories in Australia.
+export enum Jurisdiction {
+  NSW = 'New South Wales',
+  VIC = 'Victoria',
+  QLD = 'Queensland',
+  SA = 'South Australia',
+  WA = 'Western Australia',
+  TAS = 'Tasmania',
+  ACT = 'Asutralian Capital Territory',
+  NT = 'Norethern Territory',
+}
+
+// Public holidays that are celebrated in all states and territories.
+export enum PublicHoliday {
+  NewYearDay = "New Year's Day",
+  AustraliaDay = 'Australia Day',
+  GoodFriday = 'Good Friday',
+  LabourDay = 'Labour Day',
+  EasterMonday = 'Easter Monday',
+  AnzacDay = 'Anzac Day',
+  KingBirthday = "Kings's Birthday",
+  ChristmasDay = 'Christmas Day',
+  Boxingday = 'Boxing Day',
+}
