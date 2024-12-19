1b3b1961a268839802a41cc9bc31042bb79d25a3
Sanam Limbu <sudosanam@gmail.com>
2024-12-19 15:21:07 +0800

    2025 public holidays data

---
+++ src/data.ts
@@ -0,0 +1,128 @@
+// Auto-generated code, do not modify.
+
+export const ACTPublicHolidays = new Map<string, string>([
+  ['20250101', "New Year's Day"],
+  ['20250127', 'Australia Day'],
+  ['20250310', 'Canberra Day'],
+  ['20250418', 'Good Friday'],
+  ['20250419', 'Easter Saturday - the day after Good Friday'],
+  ['20250420', 'Easter Sunday'],
+  ['20250421', 'Easter Monday'],
+  ['20250425', 'ANZAC Day'],
+  ['20250602', 'Reconciliation Day'],
+  ['20250609', "King's Birthday"],
+  ['20251006', 'Labour Day'],
+  ['20251225', 'Christmas Day'],
+  ['20251226', 'Boxing Day'],
+]);
+
+export const NSWPublicHolidays = new Map<string, string>([
+  ['20250101', "New Year's Day"],
+  ['20250127', 'Australia Day'],
+  ['20250418', 'Good Friday'],
+  ['20250419', 'Easter Saturday'],
+  ['20250420', 'Easter Sunday'],
+  ['20250421', 'Easter Monday'],
+  ['20250425', 'Anzac Day'],
+  ['20250609', "King's Birthday"],
+  ['20250804', 'Bank Holiday'],
+  ['20251006', 'Labour Day'],
+  ['20251225', 'Christmas Day'],
+  ['20251226', 'Boxing Day'],
+]);
+
+export const NTPublicHolidays = new Map<string, string>([
+  ['20250101', "New Year's Day"],
+  ['20250127', 'Australia Day'],
+  ['20250418', 'Good Friday'],
+  ['20250419', 'Easter Saturday'],
+  ['20250420', 'Easter Sunday'],
+  ['20250421', 'Easter Monday'],
+  ['20250425', 'Anzac Day'],
+  ['20250505', 'May Day'],
+  ['20250609', "King's Birthday"],
+  ['20250704', 'Alice Springs Show Day'],
+  ['20250711', 'Tennant Creek Show Day'],
+  ['20250718', 'Katherine Show Day'],
+  ['20250725', 'Darwin Show Day'],
+  ['20250804', 'Picnic Day'],
+  ['20251224', 'Christmas Eve - part day holiday'],
+  ['20251225', 'Christmas Day'],
+  ['20251226', 'Boxing Day'],
+  ['20251231', "New Year's Eve - part day holiday"],
+]);
+
+export const QLDPublicHolidays = new Map<string, string>([
+  ['20250101', "New Year's Day"],
+  ['20250127', 'Australia Day'],
+  ['20250418', 'Good Friday'],
+  ['20250419', 'The day after Good Friday'],
+  ['20250420', 'Easter Sunday'],
+  ['20250421', 'Easter Monday'],
+  ['20250425', 'Anzac Day'],
+  ['20250505', 'Labour Day'],
+  ['20250813', 'Royal Queensland Show (Brisbane area only)'],
+  ['20251006', "King's Birthday"],
+  ['20251224', 'Christmas Eve (24 December) 6pm - midnight'],
+  ['20251225', 'Christmas Day'],
+  ['20251226', 'Boxing Day'],
+]);
+
+export const SAPublicHolidays = new Map<string, string>([
+  ['20250101', 'New Years Day'],
+  ['20250127', 'Australia Day public holiday'],
+  ['20250310', 'Adelaide Cup Day'],
+  ['20250418', 'Good Friday'],
+  ['20250419', 'Easter Saturday'],
+  ['20250420', 'Easter Sunday'],
+  ['20250421', 'Easter Monday'],
+  ['20250425', 'ANZAC Day'],
+  ['20250609', 'Kings Birthday'],
+  ['20251006', 'Labour Day'],
+  ['20251224', 'Christmas Eve: 7pm to midnight'],
+  ['20251225', 'Christmas Day'],
+  ['20251226', 'Proclamation Day holiday and Boxing Day'],
+  ['20251231', 'New Years Eve: 7pm to midnight'],
+]);
+
+export const TASPublicHolidays = new Map<string, string>([
+  ['20250101', "New Year's Day"],
+  ['20250127', 'Australia Day'],
+  ['20250310', 'Eight Hours Day'],
+  ['20250418', 'Good Friday'],
+  ['20250421', 'Easter Monday'],
+  ['20250422', 'Easter Tuesday - generally Tasmanian Public Service only'],
+  ['20250425', 'ANZAC Day'],
+  ['20250609', "King's Birthday"],
+  ['20251225', 'Christmas Day'],
+  ['20251226', 'Boxing Day'],
+]);
+
+export const VICPublicHolidays = new Map<string, string>([
+  ['20250101', "New Year's Day"],
+  ['20250127', 'Australia Day'],
+  ['20250310', 'Labour Day'],
+  ['20250418', 'Good Friday'],
+  ['20250419', 'Saturday before Easter Sunday'],
+  ['20250420', 'Easter Sunday'],
+  ['20250421', 'Easter Monday'],
+  ['20250425', 'ANZAC Day'],
+  ['20250609', "King's Birthday"],
+  ['20251104', 'Melbourne Cup'],
+  ['20251225', 'Christmas Day'],
+  ['20251226', 'Boxing Day'],
+]);
+
+export const WAPublicHolidays = new Map<string, string>([
+  ['20250101', "New Year's Day"],
+  ['20250127', 'Australia Day'],
+  ['20250303', 'Labour Day'],
+  ['20250418', 'Good Friday'],
+  ['20250420', 'Easter Sunday'],
+  ['20250421', 'Easter Monday'],
+  ['20250425', 'Anzac Day'],
+  ['20250602', 'Western Australia Day'],
+  ['20250929', "King's Birthday"],
+  ['20251225', 'Christmas Day'],
+  ['20251226', 'Boxing Day'],
+]);
